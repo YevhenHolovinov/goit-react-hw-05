a7083f9f83258952c23056937ef5e2406b979c70
unknown <golovinov817@gmail.com>
2025-04-07 21:16:24 +0200

    Homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import 'modern-normalize';
+import App from './components/App';
 import './index.css';
-import App from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
---
+++ src/pages/Products.jsx
@@ -0,0 +1,24 @@
+import { ProductList } from '../components/ProductList';
+import { getProducts } from '../fakeApi';
+
+export default function Products() {
+  const products = getProducts();
+
+  return (
+    <main>
+      <ProductList products={products} />
+    </main>
+  );
+}
+// import { ProductList } from '../components/ProductList';
+// import { getProducts } from '../fakeApi';
+
+// export default function Products() {
+//   const products = getProducts();
+
+//   return (
+//     <main>
+//       <ProductList products={products} />
+//     </main>
+//   );
+// }
