fb1ec9ed677cf78d9809f346eca9eb470299a282
unknown <golovinov817@gmail.com>
2025-04-27 18:08:37 +0200

    home/1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import AppHeader from '../AppHeader/AppHeader'
+import css from './App.module.css';
+
+export default function App  ()  {
+  return (
+    <div className={css.container}>
+      <AppHeader /> 
+    </div>
+  );
+};
